d9ad866348dc6ae3ae4077accfd61154ea217f1a
thanhit08 <thanhit08@msn.com>
2015-01-23 21:12:07 +0900

    Add more brick

---
+++ Lego Rework/src/magiclab/lego/brick/object/Brick_1x2x5.java
@@ -0,0 +1,12 @@
+package magiclab.lego.brick.object;
+
+import magiclab.lego.brick.Brick;
+
+public class Brick_1x2x5 extends Brick {
+	public Brick_1x2x5() {
+		// TODO Auto-generated constructor stub
+		super();
+		modelName = "Brick_1x2x5";
+		super.LoadModel(modelName);
+	}
+}
---
+++ Lego Rework/src/magiclab/lego/brick/object/Brick_2x10.java
@@ -0,0 +1,12 @@
+package magiclab.lego.brick.object;
+
+import magiclab.lego.brick.Brick;
+
+public class Brick_2x10 extends Brick {
+	public Brick_2x10() {
+		// TODO Auto-generated constructor stub
+		super();
+		modelName = "Brick_2x10";
+		super.LoadModel(modelName);
+	}
+}
